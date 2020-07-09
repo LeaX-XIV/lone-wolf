5ca9059ce86cfc23020fb4e624a5b9e1759262fb
Alessandro Borione <leax_xiv@me.com>
2020-07-09 23:18:07 +0200

    Updates

---
+++ precache-manifest.78de6e4d87a37a70841e37d6a7ace6c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a07cc7d537c3f6f8d91b14c3ac4d3b2",
+    "url": "/lone-wolf/index.html"
+  },
+  {
+    "revision": "05531d582e2fe406b9ee",
+    "url": "/lone-wolf/static/css/main.6a934dfc.chunk.css"
+  },
+  {
+    "revision": "ed5e6e38148539d9f46c",
+    "url": "/lone-wolf/static/js/2.5503bb28.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lone-wolf/static/js/2.5503bb28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05531d582e2fe406b9ee",
+    "url": "/lone-wolf/static/js/main.e21d345a.chunk.js"
+  },
+  {
+    "revision": "3fe20b0e851cbef1279f",
+    "url": "/lone-wolf/static/js/runtime-main.a4927c5d.js"
+  }
+]);
